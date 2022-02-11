c3290c9f84c09513340869e447f542241590e42d
Arya Amin <88593404+aryaamin@users.noreply.github.com>
2022-02-11 19:54:58 +0530

    Update Assignment1_Code2.py

--- Assignment1_Code2.py
+++ Assignment1_Code2.py
@@ -25,6 +25,13 @@ o = []
 x = []
 y = []
 
+h = []
+v = []
+
+for i in range(dim):
+    h.append(False)
+    v.append(False)
+
 
 for i in range(2, lines):
     o.append(int(inputtext[i][0]))
@@ -36,25 +43,26 @@ cars = lines-2 #cars+mines-redcar
 
 H = [ [ [ Bool("h_%s_%s_%s" % (i, j, k)) for k in range(k1) ] for j in range(dim) ] for i in range(dim) ]
 V = [ [ [ Bool("v_%s_%s_%s" % (i, j, k)) for k in range(k1) ] for j in range(dim) ] for i in range(dim) ]
-R = [ [ [ Bool("r_%s_%s_%s" % (i, j, k)) for k in range(k1) ] for j in range(dim) ] for i in range(dim) ]
-M = [ [ [ Bool("m_%s_%s_%s" % (i, j, k)) for k in range(k1) ] for j in range(dim) ] for i in range(dim) ]
+M = [ [ Bool("m_%s_%s" % (i, j)) for j in range(dim) ] for i in range(dim) ]
 
 for i in range(dim):
     for j in range(dim):
         H[i][j][0] = False
         V[i][j][0] = False
-        M[i][j][0] = False
-        R[i][j][0] = False
+        M[i][j] = False
 
-R[rx][ry][0] = True
+H[rx][ry][0] = True
+h[rx] = True
 
 for w in range(cars):
     if(o[w] == 0): 
         V[x[w]][y[w]][0] = True 
+        v[y[w]] = True
     elif(o[w] == 1): 
         H[x[w]][y[w]][0] = True   
+        h[x[w]] = True
     else: 
-        M[x[w]][y[w]][0] = True
+        M[x[w]][y[w]] = True
 
 
 def atleast_one(literals):
@@ -90,36 +98,21 @@ def updateV(k, i, j):
     return And(temp)
 
 
-def updateR(k, i, j):
-    temp = []
-    for p in range(dim):
-        for q in range(dim):
-            if(((p == i) and (q == (j-1))) or ((p == i) and (q == j))):
-                temp = temp #donothing
-            else:
-                temp += [R[p][q][k+1] == R[p][q][k]]
-    return And(temp)
 
-
-
-def updateVRM(k):
+def updateVM(k):
     temp = []
     for p in range(dim):
         for q in range(dim):
             temp += [V[p][q][k+1] == V[p][q][k]]
-            temp += [R[p][q][k+1] == R[p][q][k]] 
-            temp += [M[p][q][k+1] == M[p][q][k]]
 
     return And(temp)
 
 
-def updateHRM(k):
+def updateHM(k):
     temp = []
     for p in range(dim):
         for q in range(dim):
             temp += [H[p][q][k+1] == H[p][q][k]] 
-            temp += [R[p][q][k+1] == R[p][q][k]]
-            temp += [M[p][q][k+1] == M[p][q][k]]
 
     return And(temp)
 
@@ -130,7 +123,6 @@ def updateHVM(k):
         for q in range(dim):
             temp += [V[p][q][k+1] == V[p][q][k]]
             temp += [H[p][q][k+1] == H[p][q][k]]
-            temp += [M[p][q][k+1] == M[p][q][k]]
 
     return And(temp)
 
@@ -139,74 +131,55 @@ for k in range (k1-1):
     temp = []
     for i in range(dim):
         for j in range(dim):
-            if j > 0:
+            if (j > 0) and h[i]:
                 temp +=   [And(H[i][j][k],
                                 Not(V[i][j-1][k]),
                                 Not(V[i-1][j-1][k]),
-                                Not(M[i][j-1][k]),
+                                Not(M[i][j-1]),
                                 Or(Not(j > 1), Not(H[i][j-2][k])),
-                                Or(Not(j > 1), Not(R[i][j-2][k])),
                                 H[i][j-1][k+1],
                                 Not(H[i][j][k+1]),
                                 updateH(k, i, j),
-                                updateVRM(k),
+                                updateVM(k),
                                 )
                             ]
 
-            if j < (dim-2):
+            if (j < (dim-2)) and h[i]:
                 temp +=   [And(H[i][j][k],
                             Not(V[i][j+2][k]),
                             Not(V[i-1][j+2][k]),
-                            Not(M[i][j+2][k]),
+                            Not(M[i][j+2]),
                             Not(H[i][j+2][k]),
-                            Not(R[i][j+2][k]),
                             H[i][j+1][k+1],
                             Not(H[i][j][k+1]),
                             updateH(k, i, j+1),
-                            updateVRM(k),
+                            updateVM(k),
                             )
             ]
-            if i>0:
+            if (i>0) and v[j]:
                 temp += [And(V[i][j][k],
                             Not(H[i-1][j][k]),
                             Not(H[i-1][j-1][k]),
-                            Not(M[i-1][j][k]),
+                            Not(M[i-1][j]),
                             Or(Not(i > 1), Not(V[i-2][j][k])),
-                            Not(R[i-1][j][k]),
-                            Not(R[i-1][j-1][k]),
                             V[i-1][j][k+1],
                             Not(V[i][j][k+1]),
                             updateV(k, i, j),
-                            updateHRM(k),                     
+                            updateHM(k),                     
                         )
                         ]
 
-            if i < (dim-2):
+            if (i < (dim-2)) and v[j]:
                 temp += [And(V[i][j][k],
                             Not(H[i+2][j][k]),
                             Not(H[i+2][j-1][k]),
-                            Not(M[i+2][j][k]),
+                            Not(M[i+2][j]),
                             Not(V[i+2][j][k]),
-                            Not(R[i+2][j][k]),
-                            Not(R[i+2][j-1][k]),
                             V[i+1][j][k+1],
                             Not(V[i][j][k+1]),
                             updateV(k, i+1, j),
-                            updateHRM(k),                       
+                            updateHM(k),                       
                             )
-                        ]
-                        
-            if j < (dim-2):
-                temp += [And(R[i][j][k],
-                            Not(V[i][j+2][k]),
-                            Not(V[i-1][j+2][k]),
-                            Not(M[i][j+2][k]),                     
-                            Not(H[i][j+2][k]),
-                            R[i][j+1][k+1],
-                            Not(R[i][j][k+1]),
-                            updateR(k, i, j+1),
-                            updateHVM(k),
-                            ) 
                         ]
              
     s.add(atmost_one(temp))
@@ -214,7 +187,7 @@ for k in range (k1-1):
     
 condcheck = []
 for k in range(k1):
-    condcheck += [R[rx][dim-2][k]]
+    condcheck += [H[rx][dim-2][k]]
 
 s.add(Or(condcheck))
 
@@ -229,15 +202,10 @@ for i in range(dim):
                 else:
                     print(i,",",j+1, sep="")
             elif((V[i][j][0] == False) and is_true(model[V[i][j][1]])):
-                if(H[i+1][j][0] == False):
+                if(V[i+1][j][0] == False):
                     print(i,",",j, sep="")
                 else:
                     print(i+1,",",j, sep="")
-            elif((R[i][j][0] == False) and is_true(model[R[i][j][1]])):
-                if(R[i][j+1][0] == False):
-                    print(i,",",j, sep="")
-                else:
-                    print(i,",",j+1, sep="")
 
 
 for k in range(2,k1):
@@ -253,8 +221,3 @@ for k in range(2,k1):
                     print(i,",",j, sep="")
                 else:
                     print(i+1,",",j, sep="")
-            elif(is_false(model[R[i][j][k-1]]) and is_true(model[R[i][j][k]])):
-                if(is_false(model[R[i][j+1][k-1]])):
-                    print(i,",",j, sep="")
-                else:
-                   print(i,",",j+1, sep="")
