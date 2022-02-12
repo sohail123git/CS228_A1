687ca3a048e07118264b73e3b083c9fc326aa30b
Arya Amin <88593404+aryaamin@users.noreply.github.com>
2022-02-12 17:57:11 +0530

    Update Assignment1_Code2.py

--- Assignment1_Code2.py
+++ Assignment1_Code2.py
@@ -2,6 +2,7 @@ from z3 import *
 import sys
 import array as arr
 from itertools import combinations
+import time
 inputtext = []
 lines = -1
 with open(sys.argv[1]) as f:
@@ -65,16 +66,6 @@ for w in range(cars):
         M[x[w]][y[w]] = True
 
 
-def atleast_one(literals):
-    return Or(literals)
-
-def atmost_one(literals):
-    c=[]
-    for pair in combinations(literals,2):
-        a, b = pair[0], pair[1]
-        c += [Or([Not(a),Not(b)])]
-    return And(c)
-
 def updateH(k, i, j):
     temp = []
     for p in range(dim):
@@ -99,68 +90,73 @@ def updateV(k, i, j):
 
     return And(temp)
 
+start_time = time.time()
 
 for k in range (k1-1):
     temp = []
     for i in range(dim):
         for j in range(dim):
-            if (j > 0) and h[i]:
-                temp +=   [And(H[i][j][k],
-                                Not(V[i][j-1][k]),
-                                Not(V[i-1][j-1][k]),
-                                Not(M[i][j-1]),
-                                Or(Not(j > 1), Not(H[i][j-2][k])),
-                                H[i][j-1][k+1],
-                                Not(H[i][j][k+1]),
-                                updateH(k, i, j),
-                                )
-                            ]
+            if (j > 0) and h[i] and (j<5):
+                if not(M[i][j-1]) and not(M[i][j]) and not(M[i][j+1]):
+                    temp +=   [And(H[i][j][k],
+                                    Not(V[i][j-1][k]),
+                                    Not(V[i-1][j-1][k]),
+                                    Or(Not(j > 1), Not(H[i][j-2][k])),
+                                    H[i][j-1][k+1],
+                                    Not(H[i][j][k+1]),
+                                    updateH(k, i, j),
+                                    )
+                                ]
 
             if (j < (dim-2)) and h[i]:
-                temp +=   [And(H[i][j][k],
-                            Not(V[i][j+2][k]),
-                            Not(V[i-1][j+2][k]),
-                            Not(M[i][j+2]),
-                            Not(H[i][j+2][k]),
-                            H[i][j+1][k+1],
-                            Not(H[i][j][k+1]),
-                            updateH(k, i, j+1),
+                if not(M[i][j]) and not(M[i][j+1]) and not(M[i][j+2]):
+                    temp +=   [And(H[i][j][k],
+                                Not(V[i][j+2][k]),
+                                Not(V[i-1][j+2][k]),
+                                Not(H[i][j+2][k]),
+                                H[i][j+1][k+1],
+                                Not(H[i][j][k+1]),
+                                updateH(k, i, j+1),
+                                )
+                            ]
+            if (i>0) and v[j] and (i<5):
+                if not(M[i-1][j]) and not(M[i][j]) and not(M[i+1][j]):
+                    temp += [And(V[i][j][k],
+                                Not(H[i-1][j][k]),
+                                Not(H[i-1][j-1][k]),
+                                Or(Not(i > 1), Not(V[i-2][j][k])),
+                                V[i-1][j][k+1],
+                                Not(V[i][j][k+1]),
+                                updateV(k, i, j),                
                             )
-            ]
-            if (i>0) and v[j]:
-                temp += [And(V[i][j][k],
-                            Not(H[i-1][j][k]),
-                            Not(H[i-1][j-1][k]),
-                            Not(M[i-1][j]),
-                            Or(Not(i > 1), Not(V[i-2][j][k])),
-                            V[i-1][j][k+1],
-                            Not(V[i][j][k+1]),
-                            updateV(k, i, j),                
-                        )
-                        ]
+                            ]
 
             if (i < (dim-2)) and v[j]:
-                temp += [And(V[i][j][k],
-                            Not(H[i+2][j][k]),
-                            Not(H[i+2][j-1][k]),
-                            Not(M[i+2][j]),
-                            Not(V[i+2][j][k]),
-                            V[i+1][j][k+1],
-                            Not(V[i][j][k+1]),
-                            updateV(k, i+1, j),                     
-                            )
-                        ]
+                if not(M[i][j]) and not(M[i+1][j]) and not(M[i+2][j]):
+                    temp += [And(V[i][j][k],
+                                Not(H[i+2][j][k]),
+                                Not(H[i+2][j-1][k]),
+                                Not(V[i+2][j][k]),
+                                V[i+1][j][k+1],
+                                Not(V[i][j][k+1]),
+                                updateV(k, i+1, j),                     
+                                )
+                            ]
              
-    s.add(atmost_one(temp))
-    s.add(atleast_one(temp))
+    s.add(Or(temp))
     
+print("--- %s seconds ---" % (time.time() - start_time))
+
 condcheck = []
 for k in range(k1):
     condcheck += [H[rx][dim-2][k]]
 
 s.add(Or(condcheck))
 
+start_time = time.time()
 result = s.check()
+print("--- %s seconds ---" % (time.time() - start_time))
+
 model = s.model()
 
 for i in range(dim):
